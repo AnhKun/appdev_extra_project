930aacf58fcd50a76d5d09179890d6b2f644ed8d
AnhKun <nganhh014@gmail.com>
2019-04-29 07:40:28 +0300

    updated

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(){
     FILE *data;
 	clearScreen();
 	printf("\033[0;0H");
-    printf("The number of words: %d\n", NumOfWord(data));
+	printf("The number of words: %d\n", NumOfWord(data));
 	printf("Barchart1 uses to display the number of words starting from letter A to Z\n");
 	printf("Barchart2 uses to display the number of letters ( A to Z) appeared in wlist.txt\n");
 	setColors(CYAN, bg(BLACK));
